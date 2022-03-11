e31ed95a6293664825ff35541b9c6e97b42e67dd
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-03-12 00:18:32 +0530

    Added: To find the maximum  in the given  list

---
+++ list/nth_maximum.py
@@ -0,0 +1,12 @@
+def maximum():
+    list = [2, 5, 11, 21, 33, 4, 3, 10]
+    list.sort()
+    n=1
+    print("The nth maximum element is", list[-n:])
+
+if __name__ == "__main__":
+   maximum()
+
+
+
+
