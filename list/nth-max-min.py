23dac45a8414f3d1b75cda3312914593a241cc2d
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-03-12 00:17:17 +0530

    Added: To find the maximum and minimum in the given  list

---
+++ list/nth-max-min.py
@@ -0,0 +1,10 @@
+def max_min():
+    list = [5, 12, 27, 6, 32, 1, 89]
+    list.sort()
+    maximum = max(list)
+    print("The maximum element is", maximum)
+    minimum = min(list)
+    print("The minimum element is", minimum)
+
+if __name__ == "__main__":
+    max_min()
